5c912d5da32497ad27edde9c3f9acfb129f96d60
Francesco <francesco.ferruccio88@gmail.com>
2020-06-22 15:43:35 +0200

    create migration, model, factories e seeds

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// GOAL: nell'ottica di quanto visto a lezione questa mattina generare prima il db (model + factory + migration + seeder) e poi le viste per eseguire una index + edit di task sulle seguenti entita':
+// Employee <-1---N-> Task
+// Per ogni employee diversi tasks, per ogni task esattamente un employee
+// Employee:
+// - firstname
+// - lastname
+// - dateOfBirth
+// - role
+// Task:
+// - name
+// - description
+// - deadline
+// N.B.: naturalmente ad ogni entita' va aggiunto il necessario per le chiavi primarie e le chiavi esterne
+// BONUS: creare il necessario anche per eseguire update + delete
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
